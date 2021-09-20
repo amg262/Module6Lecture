90e582de2785d63fc93e0b5b11c5d52e59006dfb
Andrew <andrewmgunn26@gmail.com>
2021-09-20 12:50:07 -0500

    trello

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Module6Lecture
 {
     class Program
     {
-        static void Main(string[] args)
+        public void ExampleOne()
         {
-            Console.WriteLine("Hello World!");
+            int value = 5;
+            string name = "Andrew";
+            List<string> names = new List<string>();
+            names.Add(name);
+        }
+
+        static void Main1(string[] args)
+        {
+            //Program program = new Program();
+            //program.ExampleOne();
+            
+            Rectangle kitchen = new Rectangle(10, 10);
+                        
         }
     }
 }
---
+++ Rectangle.cs
@@ -0,0 +1,37 @@
+﻿namespace Module6Lecture
+{
+    public class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public void SetLength(double len)
+        {
+            length = len;
+        }
+        
+        /*
+         * Properties
+         */
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        /*
+         * Properties
+         */
+        public double Width
+        {
+            get => width;
+            set => width = value;
+        }
+    }
+}
